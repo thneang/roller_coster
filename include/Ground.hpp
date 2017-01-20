6205441b59f3fe5fe346c73fdc0dee29349cfe7c
kev <kevinblondel@msn.com>
2017-01-20 16:28:19 +0100

    test commit

--- include/Ground.hpp
+++ include/Ground.hpp
@@ -8,15 +8,18 @@
 #include <vector>
 
 #include <glimac/common.hpp>
-
-class Ground {
-    Ground(GLfloat width, GLfloat heigth, GLsizei discLat, GLsizei discLong):
-    m_nVertexCount(0) {
-    }
-private:
-    std::vector<ShapeVertex> m_Vertices;
-    GLsizei m_nVertexCount; // Nombre de sommets
-};
-
+namespace glimac {
+    class Ground {
+    public:
+        void build(GLfloat width, GLfloat height);
+        Ground(GLfloat width, GLfloat height) :
+                m_nVertexCount(0) {
+            build(width,height);
+        }
+    private:
+        std::vector<ShapeVertex> m_Vertices;
+        GLsizei m_nVertexCount; // Nombre de sommets
+    };
+}
 
 #endif //ROLLER_COSTER_GROUND_HPP
--- src/Ground.cpp
+++ src/Ground.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Ground.hpp"
+namespace glimac {
+    void Ground::build(GLfloat width, GLfloat height) {
+
+        std::vector<ShapeVertex> data;
+        for(int i=0;i<6;i++){
+            ShapeVertex vertex;
+            vertex.texCoords.x = i * rcpLat;
+            vertex.texCoords.y = 1.f - j * rcpLong;
+
+            vertex.normal.x = sin(i * dPhi) * cosTheta;
+            vertex.normal.y = sinTheta;
+            vertex.normal.z = cos(i * dPhi) * cosTheta;
+
+            vertex.position = r * vertex.normal;
+            data.push_back(vertex);
+        }
+    }
+}
