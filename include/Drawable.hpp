89c6806adfa7e050f92a943200f3e422443cc505
thomas <thneang@gmail.com>
2017-01-21 19:44:48 +0100

    Ajout de methode init + champ pour classe abstraitre drawable

--- include/Drawable.hpp
+++ include/Drawable.hpp
@@ -4,18 +4,52 @@
 #include <iostream>
 
 using namespace glimac;
+using namespace std;
 
 class Drawable {
-public :
+protected :
 
-    GLuint texture;
+    // ensemble des "ID" de texture
+    GLuint *texture;
+    // "ID" du buffer de vertex
     GLuint vbo;
+    //"ID" du buffer attribut
     GLuint vao;
-    // Init l'objet, envoie les données au gpu et demarre un programme pour les shaders
+
+    // Contient les "ID" de nos meshes
+    GLuint elements[2];
+
+    // Nos images utilisé par les textures
+    // TODO passer en pointeur pour plusieurs textures
+    unique_ptr<Image> images[1];
+
+    // Program pour utiliser les shaders
+    Program program;
+
+public :
+    // Init l'objet (allocation), envoie les données au gpu et demarre un programme pour les shaders
     virtual void init(const FilePath& filepath = nullptr) = 0;
+
+    // envoie l'image au gpu
+    virtual void init_texture() = 0;
+
+    // envoie les sommets au gpu
+    virtual void init_vbo() = 0;
+
+    // decrit les données pour le gpu
+    virtual void init_vao() = 0;
+
+    // permet d'utiliser l'indexation des sommets pour draw
+    // cela permet de d'éviter de duppliquer des sommets
+    // lorsqu'on dessine nos triangles pour les objets
+    virtual void init_index() {
+    }
 
     // Demande de dessin a openGL
     virtual void draw() = 0;
+
+    // libere l'espace allouée, doit être appelé à la fin du programme
+    virtual void free() = 0;
 
 };
 
