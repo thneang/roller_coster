20f98b72acb5de964a683885addee643b0a21432
thomas <thneang@gmail.com>
2017-01-21 19:43:46 +0100

    Variable global pour opengl

---
+++ include/GL_global.hpp
@@ -0,0 +1,13 @@
+#ifndef ROLLER_COSTER_GL_GLOBAL_HPP
+#define ROLLER_COSTER_GL_GLOBAL_HPP
+const static GLint VERTEX_ATTRIB_POSITION = 0;
+const static GLint VERTEX_ATTRIB_POSITION_SIZE = 3;
+const static GLint VERTEX_ATTRIB_NORMAL = 1;
+const static GLint VERTEX_ATTRIB_NORMAL_SIZE = 3;
+const static GLint VERTEX_ATTRIB_TEXTURE_COORD = 2;
+const static GLint VERTEX_ATTRIB_TEXTURE_COORD_SIZE = 2;
+//static GLint WIN_WIDTH = 800;
+//static GLint WIN_HEIGTH = 600;
+//static float NEAR = 0.1f;
+//static float FAR = 100.f;
+#endif //ROLLER_COSTER_GL_GLOBAL_HPP
