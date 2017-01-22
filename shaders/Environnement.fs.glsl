24f353904d8cf78b12bc983783bb3c5b70e463fd
thomas <thneang@gmail.com>
2017-01-22 01:06:44 +0100

    shader avec gestion matrice

--- shaders/Environnement.fs.glsl
+++ shaders/Environnement.fs.glsl
@@ -1,9 +1,13 @@
 #version 330
 
-in vec3 vColor;
+in vec3 vPosition_vs;
+in vec3 vNormal_vs;
+in vec2 vTexCoords;
 
 out vec3 fFragColor;
 
+uniform sampler2D uTexture;
+
 void main() {
-    fFragColor = vColor;
+    fFragColor = vec3(255,255,255);
 }
